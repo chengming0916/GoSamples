c3f6f918f98dbe88c1fbe67479c97e30a696496e
Chemmy <chengming0916@outlook.com>
2025-05-09 10:49:23 +0800

    添加多种日志库使用示例

--- LogSample/go.mod
+++ LogSample/go.mod
@@ -5,11 +5,19 @@ go 1.24.1
 require (
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/sirupsen/logrus v1.9.3
+	go.uber.org/zap v1.27.0
+	k8s.io/klog/v2 v2.130.1
 )
 
 require (
+	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/jonboulle/clockwork v0.5.0 // indirect
 	github.com/lestrrat-go/strftime v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+)
+
+require (
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
--- LogSample/pkg/log/log.go
+++ LogSample/pkg/log/log.go
@@ -6,8 +6,14 @@ import (
 	"runtime"
 	"time"
 
+	"log/slog"
+
 	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"k8s.io/klog/v2"
 )
 
 func init() {
@@ -49,5 +55,64 @@ func init() {
 		//rotatelogs.WithRotationSize(5*1204),
 		rotatelogs.WithRotationTime(time.Duration(60)*time.Second), // 设置日志分割的时间，隔多久分割一次
 	)
+
 	logrus.SetOutput(io.MultiWriter(os.Stdout, writer))
+
+	writer1 := &lumberjack.Logger{
+		Filename:   filePath,
+		MaxSize:    500, // 日志文件大小,单位MB
+		MaxBackups: 3,   // 最多保留日志文件数
+		MaxAge:     30,  // 最大保存天数
+	}
+
+	// v1.21版本官方日志库
+	handler := slog.NewTextHandler(writer1, &slog.HandlerOptions{Level: slog.LevelDebug})
+	// handler := slog.NewJSONHandler(writer1, &slog.HandlerOptions{Level: slog.LevelDebug}) // Json
+	slog.SetDefault(slog.New(handler))
+
+	// klog
+	klog.InitFlags(nil) // 初始化全局标志
+	klog.SetOutput(io.MultiWriter(os.Stdout, writer1))
+
+	// zap.NewProduction() // 输出Json日志，调试级别不输出
+	// zap.NewDevelopment() // 输出行日志
+	// log := zap.NewExample() // 输出Json日志
+	// log.Info("", zap.Int("age", 4), zap.String("gender", "M")) // 记录结构化日志
+	// 以printf格式记录语句
+	// log.Sugar().Debug() //
+	// log.Sugar().Desugar() // 从sugar logger切换到标准记录器
+
+	// encoderConfig := zap.NewProductionEncoderConfig()
+	// encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder   // 修改时间编码器
+	// encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder // 使用大写标记日志级别
+	// encoder := zapcore.NewConsoleEncoder(encoderConfig)
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "linenum",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder, // 全路径
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+
+	// 设置日志级别
+	level := zap.NewAtomicLevel()
+	level.SetLevel(zap.InfoLevel)
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(writer1)), // 输出到控制台和文件
+		level, // 日志级别
+	)
+
+	logger := zap.New(core, zap.AddCaller()) // AddCaller 将调用函数信息记录到日志中（堆栈跟踪）
+	// sugarLogger := logger.Sugar()
+	defer logger.Sync() // 确保日志缓冲区被刷新
 }
